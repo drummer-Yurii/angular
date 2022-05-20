73ca98a62e31ef3e0857c663727232911efebfd0
drummer-Yurii <104525685+drummer-Yurii@users.noreply.github.com>
2022-05-20 18:27:21 +0300

    elastic email

--- back/services/all/mail.service.js
+++ back/services/all/mail.service.js
@@ -1,66 +1,35 @@
-// https://myaccount.google.com/lesssecureapps 
-import nodemailer from 'nodemailer';
 import { log } from '../../colub/high-level/index.js';
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+import elasticemail from 'elasticemail';
+
 
 class MailService {
     constructor() { }
     async init() {
+        this.client = elasticemail.createClient({
+            username: 'uriirybachok@gmail.com',
+            apiKey: process.env.NODEMAILER_API_KEY
+        });
         log('init')
-        this.transporter = nodemailer.createTransport({
-            port: 2525,
-            // secure: false, // true for 587, false for other ports
-            // requireTLS: true,
-            // service: 'gmail',
-            host: 'smtp.elasticemail.com',
-            // port: 465,
-            // secure: true,
 
-            username: process.env.GMAIL_MAIL,
-            apiKey: process.env.GMAIL_PASS
-            // tls: {
-            //     rejectUnauthorized: false
-            // },
-        })
-        // api key .................пошукати
-        // elasticmail example......пошукати і шукати приклад коду
-
-        // this.transporter = nodemailer.createTransport({
-        // host: 'smtp.gmail.com',
-        //     port: 465,
-        //     secure: true,
-        //     auth: {
-        //         user: process.env.GMAIL_MAIL,
-        //         pass: process.env.GMAIL_PASS,
-        //     },
-        //     tls: {
-        //         rejectUnauthorized: false
-        //     },
-        //     connectionTimeout: 1 * 20 * 1000
-        // });
     }
     async send() {
         log('send')
         await this.init();
-        const set = {
-            from: 'me',
+        var msg = {
+            from: 'uriirybachok@gmail.com',
+            from_name: 'Urii',
             to: 'drummer2009@ukr.net',
-            subject: 'mail confirmation',
-            html: `
-                 <p>
-                 To verify your mail, go to this link. 
-                 <a href="" target="_blank">link</a>
-                 </p>
-            `
+            subject: 'Hello',
+            body_text: 'Hello World!'
         };
-        // this.transporter.sendMail(set, this.after);
-        this.transporter.sendMail(set, async (err, res) => {
-            log(res, 'after', err)
-            err
-                ? { ok: false, msg: 'Can not send Email!', err }
-                : { ok: true, msg: 'Email Sent' };
-        });
 
+        this.client.mailer.send(msg, function (err, result) {
+            if (err) {
+                return console.error(err);
+            }
+
+            console.log(result);
+        });
     }
     async after(err, res) {
         log(err, res)
