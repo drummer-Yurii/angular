83ee23d5b8316ff3c51fbb936a00bb91bfe376bd
drummer-Yurii <104525685+drummer-Yurii@users.noreply.github.com>
2022-06-09 12:18:46 +0300

    post design

--- vue-project/src/components/Post.vue
+++ vue-project/src/components/Post.vue
@@ -1,5 +1,12 @@
+@ -1,92 +0,0 @@
 <template>
-      <div class="post"></div>
+  <div class="post" v-for="post in posts">
+    <img :src="post.img" />
+    <div class="substrate"></div>
+    <h1>{{post.title}}</h1>
+    </div>
+
+
 
 </template>
 
@@ -24,7 +31,20 @@ export default {
   },
   data() {
     return {
-      userData: {}
+      posts: [
+        {
+          img:'/src/assets/1.webp',
+          title:'bech'
+        },
+         {
+          img:'/src/assets/1.webp',
+          title:'bech'
+        },
+         {
+          img:'/src/assets/1.webp',
+          title:'bech'
+        }
+      ]
     }
   },
   created() {
@@ -88,5 +108,28 @@ export default {
   background: gray;
   margin-top: 2rem;
   border-radius: 3rem;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  overflow: hidden;
+  position: relative;
+  /* background: url(@/assets/1.webp);
+  background-size: cover; */
+}
+
+.post>img {
+  width: 100%;
+}
+.post>h1 {
+  position: absolute;
+  left: 1rem;
+  bottom: 0;
+  color: wheat;
+}
+.substrate{
+  box-shadow: inset -24px -30px 23px rgba(0, 0, 0, 0.7);
+height: 10rem;
+position: absolute;
+left: 0;
+bottom: 0;
+width: 100%;
 }
 </style>
---
+++ vue-project/src/components/_Example.vue
@@ -0,0 +1,88 @@
+<template>
+
+  <section>
+    <div class="">
+  
+    </div>
+  </section>
+
+</template>
+
+<script>
+import { ref } from 'vue'
+import axios from 'axios'
+import { useUserStore } from '@/stores/user'
+import { useAppStore } from '@/stores/app'
+import  from '@/components/.vue'
+
+// import {config} from '@/my-config'
+
+export default {
+  components:{
+    
+  },
+  // props: {
+    // companyName: String,
+  // },
+  setup() {
+    const storeUser = useUserStore()
+    const storeApp = useAppStore()
+    return {
+      storeUser,
+      storeApp
+    }
+  },
+  data() {
+    return {
+      // userData: {}
+    }
+  },
+  created() {
+    // this.getUserData()
+    // this.getAvatar()
+  },
+  methods: {
+    // getUserData() {
+    //   axios
+    //     .get('http://localhost:3001/api/user', {
+    //       headers: {
+    //         'auth-token': localStorage.getItem('authToken'),
+    //       }
+    //     })
+    //     .then((answer) => {
+    //       console.log(answer)
+    //       this.userData = answer.data.user
+    //       this.storeUser.update(answer.data.user)
+    //       if (this.storeApp.goToAnotherPageAfterReload) {
+    //         if (this.storeUser.user.username == 'admin') this.$router.push('/admin/main')
+    //         else this.$router.push('/')
+    //       }
+    //     })
+
+    // },
+   
+  
+    // editProfile() {
+    //   console.log('editProfile')
+    //   axios
+    //    .put('http://localhost:3001/api/user', this.userData, {
+    //      'auth-token': localStorage.getItem('authToken')
+    //    })
+    //    .then((answer) => {
+    //       console.log(answer)
+    //       this.getUserData()
+    //       // this.userData=answer.data.user
+    //     })
+    // },
+  }
+}
+</script>
+
+<style scoped>
+@import url(//fonts.googleapis.com/css?family=Lato:300:400);
+
+section {
+  text-align: center;
+  z-index: 10;
+}
+</style>
