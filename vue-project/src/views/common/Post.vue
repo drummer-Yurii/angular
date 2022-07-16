cc26a55e6610b7e0a8b82d81ee14207ad43fe4bb
drummer-Yurii <104525685+drummer-Yurii@users.noreply.github.com>
2022-07-16 11:59:06 +0300

    share to facebooke link

--- vue-project/src/views/common/Post.vue
+++ vue-project/src/views/common/Post.vue
@@ -46,6 +46,7 @@
 
       <div class="post-panel">
         <div class="panel-icon facebook-icon">
+          <a :href="fbShareLink">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
             role="img" class="iconify iconify--ic" width="32" height="32" preserveAspectRatio="xMidYMid meet"
             viewBox="0 0 24 24">
@@ -53,6 +54,7 @@
               d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z">
             </path>
           </svg>
+          </a>
         </div>
       </div>
 
@@ -74,10 +76,17 @@ export default {
     const storePost = usePostStore();
     const storeUser = useUserStore();
     const route = useRoute();
+    const pageLink = location.href;
+    // const fbShareLink = 'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.nytimes.com%2Fes%2F2020%2F05%2F26%2Fespanol%2Fcoronavirus-superficies-como-se-transmite.html&amp%3Bsrc=sdkpreparse'
+    const fbShareLink = 'https://www.facebook.com/sharer/sharer.php?u='+pageLink+'&src=sdkpreparse&t=TITLE&display=popup&quote=TEXT&hashtag=#HASHTAG'
+    log(pageLink);
+    log(fbShareLink)
     return {
       storePost,
       storeUser,
       route,
+      pageLink,
+      fbShareLink
     };
   },
   async created() {
